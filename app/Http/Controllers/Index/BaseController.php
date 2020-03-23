f01e9b87186b068226ba768fb96ab2efdf857316
kuan <woaini3900098@qq.com>
2020-03-23 15:30:47 +0800

    调试

--- app/Http/Controllers/Index/BaseController.php
+++ app/Http/Controllers/Index/BaseController.php
@@ -8,15 +8,15 @@ use App\Http\Controllers\Controller;
 class BaseController extends Controller
 {
 	public function curl_get_https($url){
-        $curl = curl_init(); // 启动一个CURL会话
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
-        //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);  // 从证书中检查SSL加密算法是否存在
-        $tmpInfo = curl_exec($curl);     //返回api的json对象
-        //关闭URL请求
-        curl_close($curl);
-        return json_decode($tmpInfo, true);    //返回json对象
+        $ch = curl_init();
+		//设置选项，包括URL
+		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		//执行并获取HTML文档内容
+		$output = curl_exec($ch);
+		//释放curl句柄
+		curl_close($ch);
+		return $output;
     }
 }
